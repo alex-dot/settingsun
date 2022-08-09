5b0d23ae6cccd1805de39371671e10ed98f2a0c5
Alexander Herr <herr@eth.mpg.de>
2022-08-09 16:32:49 +0200

    running street

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 
 #include "bn_regular_bg_items_road.h"
 #include "bn_bg_palette_items_road_palette.h"
+#include "bn_bg_palette_items_road_palette_alt.h"
+#include "bn_sprite_items_tarmac.h"
 
 namespace {
     void main_scene(bn::sprite_text_generator& text_generator) {
@@ -29,9 +31,90 @@ namespace {
         int counter = 1;
 
         bn::regular_bg_ptr road_bg = bn::regular_bg_items::road.create_bg(0, 0);
-        const bn::bg_palette_item& road_original_palette = bn::regular_bg_items::road.palette_item();
-        const bn::bg_palette_item& road_alt_palette = bn::bg_palette_items::road_palette;
-        bn::bg_palette_ptr road_palette = road_bg.palette();
+        const bn::bg_palette_item& road_org_palette = bn::bg_palette_items::road_palette;
+        const bn::bg_palette_item& road_alt_palette = bn::bg_palette_items::road_palette_alt;
+        bn::bg_palette_ptr road_palette_ptr = road_bg.palette();
+
+        float current_shear = 0.2;
+
+        float scale = 0.75;
+        bn::sprite_ptr tarmac1l = bn::sprite_items::tarmac.create_sprite(-48,68);
+        tarmac1l.set_scale(1, scale);
+        tarmac1l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac1r = bn::sprite_items::tarmac.create_sprite(48,68);
+        tarmac1r.set_scale(1, scale);
+        tarmac1r.set_horizontal_flip(true);
+        tarmac1r.set_horizontal_shear(current_shear);
+
+        scale = 0.5;
+        int l = abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac2l = bn::sprite_items::tarmac.create_sprite(-48 + l,48);
+        tarmac2l.set_scale(1, scale);
+        tarmac2l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac2r = bn::sprite_items::tarmac.create_sprite(48 - l,48);
+        tarmac2r.set_scale(1, scale);
+        tarmac2r.set_horizontal_flip(true);
+        tarmac2r.set_horizontal_shear(current_shear);
+
+        scale = 0.375;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac3l = bn::sprite_items::tarmac.create_sprite(-48 + l,34);
+        tarmac3l.set_scale(1, scale);
+        tarmac3l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac3r = bn::sprite_items::tarmac.create_sprite(48 - l,34);
+        tarmac3r.set_scale(1, scale);
+        tarmac3r.set_horizontal_flip(true);
+        tarmac3r.set_horizontal_shear(current_shear);
+
+        scale = 0.25;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac4l = bn::sprite_items::tarmac.create_sprite(-48 + l,24);
+        tarmac4l.set_scale(1, scale);
+        tarmac4l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac4r = bn::sprite_items::tarmac.create_sprite(48 - l,24);
+        tarmac4r.set_scale(1, scale);
+        tarmac4r.set_horizontal_flip(true);
+        tarmac4r.set_horizontal_shear(current_shear);
+
+        scale = 0.25;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac5l = bn::sprite_items::tarmac.create_sprite(-48 + l,16);
+        tarmac5l.set_scale(1, scale);
+        tarmac5l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac5r = bn::sprite_items::tarmac.create_sprite(48 - l,16);
+        tarmac5r.set_scale(1, scale);
+        tarmac5r.set_horizontal_flip(true);
+        tarmac5r.set_horizontal_shear(current_shear);
+
+        scale = 0.125;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac6l = bn::sprite_items::tarmac.create_sprite(-48 + l,10);
+        tarmac6l.set_scale(1, scale);
+        tarmac6l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac6r = bn::sprite_items::tarmac.create_sprite(48 - l,10);
+        tarmac6r.set_scale(1, scale);
+        tarmac6r.set_horizontal_flip(true);
+        tarmac6r.set_horizontal_shear(current_shear);
+
+        scale = 0.125;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac7l = bn::sprite_items::tarmac.create_sprite(-48 + l,6);
+        tarmac7l.set_scale(1, scale);
+        tarmac7l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac7r = bn::sprite_items::tarmac.create_sprite(48 - l,6);
+        tarmac7r.set_scale(1, scale);
+        tarmac7r.set_horizontal_flip(true);
+        tarmac7r.set_horizontal_shear(current_shear);
+
+        scale = 0.125;
+        l = l + abs(32 * scale * current_shear) + 1;
+        bn::sprite_ptr tarmac8l = bn::sprite_items::tarmac.create_sprite(-48 + l,2);
+        tarmac8l.set_scale(1, scale);
+        tarmac8l.set_horizontal_shear(current_shear);
+        bn::sprite_ptr tarmac8r = bn::sprite_items::tarmac.create_sprite(48 - l,2);
+        tarmac8r.set_scale(1, scale);
+        tarmac8r.set_horizontal_flip(true);
+        tarmac8r.set_horizontal_shear(current_shear);
 
         BN_LOG(bn::display::height());
         BN_LOG(-bn::display::height() / 2);
@@ -42,13 +125,13 @@ namespace {
             max_cpu_usage = bn::max(max_cpu_usage, bn::core::last_cpu_usage());
             --counter;
 
-            if(! counter) {
-                bn::string<32> text;
-                bn::ostringstream text_stream(text);
-                text_stream.append((max_cpu_usage * 100).right_shift_integer());
-                text_stream.append("%");
+            if( counter == 0 ) {
+                bn::string<32> fps_text;
+                bn::ostringstream fps_text_stream(fps_text);
+                fps_text_stream.append((max_cpu_usage * 100).right_shift_integer());
+                fps_text_stream.append("%");
                 text_sprites.clear();
-                text_generator.generate(-112, -72, text, text_sprites);
+                text_generator.generate(-112, -72, fps_text, text_sprites);
 
                 max_cpu_usage = 0;
                 counter = 60;
@@ -80,20 +163,20 @@ namespace {
             }
 
             if( speed != 0 && counter % bg_move == 0 ) {
-              if(road_palette.colors() == road_original_palette.colors_ref()) {
-                  road_palette.set_colors(road_alt_palette);
+              if(road_palette_ptr.colors() == road_org_palette.colors_ref()) {
+                  road_palette_ptr.set_colors(road_alt_palette);
               }
               else {
-                  road_palette.set_colors(road_original_palette);
+                  road_palette_ptr.set_colors(road_org_palette);
               }
             }
 
-            bn::string<32> text;
-            bn::ostringstream text_stream(text);
-            text_stream.append(speed);
-            text_stream.append(" km/h");
-            text_sprites.clear();
-            text_generator.generate(64, -72, text, text_sprites);
+//            bn::string<32> text;
+//            bn::ostringstream text_stream(text);
+//            text_stream.append(speed);
+//            text_stream.append(" km/h");
+//            text_sprites.clear();
+//            text_generator.generate(64, -72, text, text_sprites);
 
             // testing basic logging
             if( bn::keypad::any_pressed() ) {
